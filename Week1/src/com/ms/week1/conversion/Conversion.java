2c9f753c706a108ece66409bcea5aff07e6c6a33
Michael <MichaelsUniversity@proton.me>
2025-09-03 11:25:08 -0400

    Converting Values

---
+++ Week1/src/com/ms/week1/conversion/Conversion.java
@@ -0,0 +1,39 @@
+package com.ms.week1.conversion;
+
+import java.util.Scanner;
+
+/**
+ * @author Michael
+ * Converting Values of Farhenheit to Celsius and Miles to Kilometers
+ */
+
+public class Conversion {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		// Farhenheit to Celsius
+		
+		System.out.println("Please enter the current temperature in Farhenheit: ");
+		
+		double far = sc.nextDouble();
+		
+		double cel = (far - 32) * 5.0 / 9.0;
+		
+		System.out.printf("%fF is %fC%n", far, cel);
+		
+		// Miles to Kilometers
+		
+		System.out.println("Please enter a number of Miles: ");
+		
+		double mile = sc.nextDouble();
+		
+		double kilo = mile * 1.60934;
+		
+		System.out.printf("%f miles is %f kilometers%n", mile, kilo);
+		
+		sc.close();
+	}
+	
+}
